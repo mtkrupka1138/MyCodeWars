dff8c0b11a6e3a9ef8aca8aa1900dc9f2a052879
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-06-03 11:33:12 -0400

    update file names and add intro comments

--- cw-0001.js
+++ cw-0001.js
@@ -1,3 +1,10 @@
+// Codewars challenge 0001
+
+// Write a function that takes in a string of one or more words, and returns 
+// the same string, but with all five or more letter words reversed. Strings 
+// passed in will consist of only letters and spaces. Spaces will be included 
+// only when more than one word is present. 
+
 function spinWords(string){
     const words = string.split(' ');
     const wordReverse = words.map((word) => {
